264cda0e8c3ec2ad80088c6ab05045ab417d0dec
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-08 17:58:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,4 +11,15 @@ class Dog < ActiveRecord::Base
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
+  def self.find_by(attribute)
+      column_name = attribute.keys[0].to_s
+      value_name = attribute.values[0]
+
+      sql = <<-SQL
+        SELECT * FROM #{table_name}
+        WHERE #{column_name} = ?
+        SQL
+
+      DB[:conn].execute(sql, value_name);
+    end
 end
