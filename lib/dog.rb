4479bf90f15960d8129bae0fa830295fc78483b5
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-08 19:37:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,3 @@
 class Dog < ActiveRecord::Base
 
-  attr_accessor :name, :breed
-
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
-
-  def self.create(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
-
-  def self.find_by(attribute)
-      column_name = attribute.keys[0].to_s
-      value_name = attribute.values[0]
-
-      sql = <<-SQL
-        SELECT * FROM #{table_name}
-        WHERE #{column_name} = ?
-        SQL
-
-      DB[:conn].execute(sql, value_name);
-    end
 end
